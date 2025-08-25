3bcd32a3fce4be4d37a68d5fc58494aef249cc0f
Nethaka Kaveen Wijerathna <nethaka2003@gmail.com>
2025-08-25 18:51:19 +0530

    Refactor hamburger menu functionality and styles for improved responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.style.maxHeight = menu.style.maxHeight ? null : menu.scrollHeight + "px";
+   menu.classList.toggle("open");
+   icon.classList.toggle("open");
 }
